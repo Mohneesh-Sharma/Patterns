f26ef0f83be77d82eaf27c4a062d71423d11ec84
Mohneesh-Sharma <vandnasharma031985@gmail.com>
2026-05-31 23:59:25 +0530

    patterns

--- star triangle right.c
+++ star triangle right.c
@@ -2,10 +2,10 @@
 int main(){
     int i,j,n;
     printf("enter number of lines :");
-    scanf("%d",n);
+    scanf("%d",&n);
     for(i=1;i<=n;i++){
         for(j=1;j<=n;j++){
-            if(j==n || j==n+1-i)
+            if(j>=n+1-i)
             printf("*");
             else printf(" ");
         }
